e11d37cdcf2b65fa33e88696c7b462d5f1c4d6e2
mdsipu080045-cell <mdsipu080045@gmail.com>
2026-01-14 16:26:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# 🗿  Visit & Support us - @UHD_NETWORK
-# ⚡️ Do Not Remove Credit - Made by @UHDBots
-# 💬 For Any Help Join Support Group: @UHDBots_Support
+# 🗿  Visit & Support us - @newmoviesabbir
+# ⚡️ Do Not Remove Credit - Made by @newmoviesabbir
+# 💬 For Any Help Join Support Group: @all_anime_in_hindi_dub1
 # 🚫 Removing or Modifying these Lines will Cause the bot to Stop Working.
 
 
@@ -12,9 +12,11 @@ id_pattern = re.compile(r'^-?\d+$')
 
 
 SESSION = environ.get("SESSION", "UHDFiletoLinksBot")
-API_ID = int(environ.get("API_ID", "0"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+# আপনার দেওয়া তথ্যগুলো নিচে বসানো হয়েছে
+API_ID = int(environ.get("API_ID", "29608422"))
+API_HASH = environ.get("API_HASH", "3db2f8e109301f02f5d9c8f10dd79244")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8560160655:AAHzkz6R3aoKeAqu_XYoxUqTJ9pmUz0QrtI")
 
 
 PORT = int(environ.get("PORT", "8080"))
@@ -22,15 +24,15 @@ MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 ON_HEROKU = "DYNO" in environ
-URL = environ.get("URL", "")
+URL = environ.get("URL", "") # এখানে আপনার Render বা Heroku অ্যাপের লিঙ্ক দিতে পারেন
 
 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003360104781"))
 ADMINS = [
     int(admin) if id_pattern.match(admin) else admin
-    for admin in environ.get("ADMINS", "").split()
+    for admin in environ.get("ADMINS", "8056243176").split()
 ]
 
 
-DATABASE_URI = environ.get("DATABASE_URI", "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mdohidh20:emETBcodRUOnB69C@movienamerequest.smfx6uk.mongodb.net/?retryWrites=true&w=majority&appName=Movienamerequest")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Movienamerequest")
